220819fb1d83a54af83cc55380954cbd35b66a49
Barun Kalita <barunkalita1993@gmail.com>
2020-01-05 04:03:18 +0530

    FInal code...finshing left

--- care.py
+++ care.py
@@ -17,15 +17,6 @@ class Young(User):
     def __repr__(self):   
   	    return "(name={},fees={})".format(self.name,self.amount)
 
-class Request:
-    def __init__(self,name):
-        self.name = name
-    def __repr__(self):
-        return "(name={})".format(self.name)
-
-class Approval:
-    def __init__(self,name):
-        self.name=name
 
 # creating list	 
 retired_list = []
@@ -33,12 +24,13 @@ young_list = []
 request_list= []
 approval_list= []
 
-print("\nREGISTRATION | VIEW  |  TICKETING")
+print("\nREGISTRATION | VIEW  |  TICKETING |  FINAL_LIST")
 print(" 1. Register your name(retired) and funds if you want to be taken care of")
 print(" 2. Register your name(young), price  if you want to take care")
-print(" 3. View the list of Retired persons and funds")
-print(" 4. View the list of Young person and price")
-print(" 5. Ticketing")
+print(" 3. View the list of registered CareSeeker person and funds")
+print(" 4. View the list of registered CareTakers and price")
+print(" 5. Selection and Approval of caretakers")
+print(" 6. Final Approved List")
 
 choice = int(input("ENTER YOUR CHOICE (1-5): "))
  
@@ -88,29 +80,47 @@ elif choice ==5:
                 print("\n\n.................List of Retired person with allocated budget........\n")
                 with open("Retired_names.txt","r+") as f :
                     print(f.read())
-                print("\nPlease select entries for approval by typing in the values")
-                ele1 = input("Enter  name : ")
-                with open("Retired_names.txt","r+") as f :
-                    
-                    if ele1 in f.read():
-                        print("Request Recorded against your Registered name")
-                        request_list.append(Request(ele1))
-                        print(request_list)
-                        with open(item,"a+") as wn :
-                            for item in request_list:
-                                wn.write("%s\n" % item)
-                    else:
-                        print("No such record found. Try again")
-                 
+                print("\nPlease select by typing in the name for approval request ")
+                print("\nENTER UPTO FOUR NAMES  WHEN PROPMTED\n")
+                i=0
+                while(i<4):
+                    input1 = input("Enter  name : ")
+                    with open("Retired_names.txt","r+") as f :
+                        if input1 in f.read():
+                            print("Request Recorded against your Registered name")
+                            request_list.append(input1)
+                            with open('request.txt',"a+") as wn :
+                                    wn.write("\n%s<------>%s" %(input1,item))
+                                    i+=1
+                        else:
+                            print("No such record found. Try again")
+                
                     
             else:
                 print("Your name is not in Registered list")
         
 
       
+    elif option ==2:
+        item = input("Please Enter your registered name: ")
+        with open('Retired_names.txt') as f:
+            if item in f.read():
+                print("\n...................WELCOME TO APPROVAL PORTAL...............")
+                print("\n\nList of CareTaker  who have opted for approval........\n")
+                with open("request.txt","r+") as fp :
+                    if item in fp.read():
+                        with open("request.txt","r+") as fp :
+                            print(fp.read())
+                        input1 = input("Enter  the caretaker name for approval : ")
+                        approval_list.append(input1)
+                        with open('approve.txt',"a+") as wn :
+                            wn.write("%s----->%s\n" %(item,input1))
+                    else:
+                        print("CareTaker pending...Please check after sometime")
+                
+            else:
+                print("Your name is not in Registered list")
 
-
-    # print("Retired person with allocated budget")
-    # with open("Retired_names.txt","r+") as f :
-    #         print(f.read())
-    # input1=input("Enter the name of the retired person:")
+elif choice ==6:
+    with open("approve.txt","r+") as f :
+        print(f.read())
